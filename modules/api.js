d40c827f0daa08abe332326223f76a72359bbdf3
Oskar Risberg <oskar.risberg@stendahls.se>
2024-03-25 09:20:44 +0100

    send proper user agent

--- modules/api.js
+++ modules/api.js
@@ -21,6 +21,7 @@ const get = function get ( requestPath, queryParams ) {
         const options = {
             headers: {
                 Authorization: `Bearer ${ API_TOKEN }`,
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
             },
             hostname: API_HOST,
             method: 'GET',
